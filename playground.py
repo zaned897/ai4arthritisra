53a521fe53c4c06a1791b478f9250139786da160
zaned897 <3dmena@gmail.com>
2024-05-16 14:09:16 -0600

    draft: documentation ignored and playground code for test

--- playground.py
+++ playground.py
@@ -1,4 +1,3 @@
-#%%
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import PyPDFLoader
 from langchain.chat_models import ChatOllama as Ollama
@@ -12,7 +11,7 @@ from time import time
 def get_pdf_files(path: str):
     return list(path.glob("*.pdf"))
 
-def load_vector_store(base_dir:str, knowledge_base_files: List[Path]) -> FAISS:
+def load_vector_store(base_dir: str, knowledge_base_files: List[Path]) -> FAISS:
     embeds_dir = base_dir / "embeds"
     embeds = OllamaEmbeddings(model='nomic-embed-text')
     splitter = RecursiveCharacterTextSplitter(
@@ -27,10 +26,10 @@ def load_vector_store(base_dir:str, knowledge_base_files: List[Path]) -> FAISS:
         if embed_base.exists():
             print(f"Loading vector store from {embed_base}")
             vector_store = FAISS.load_local(
-                base_dir=embed_base,
+                folder_path=embed_base,
                 embeddings=embeds,
                 allow_dangerous_deserialization=True
-                )
+            )
             database.append(vector_store)
         else:
             print(f"Creating vector store from {file}")
@@ -44,20 +43,29 @@ def load_vector_store(base_dir:str, knowledge_base_files: List[Path]) -> FAISS:
         database[0].merge_from(db)
     return database[0]
 
-knowledge_bas_path = Path("docs/resources")
-embeds_path = knowledge_bas_path / "embeds"
+def interact_with_pdfs(chain, query: str):
+    return chain.run(query)
 
-knowledge_base_files = get_pdf_files(knowledge_bas_path)
-vector_store = load_vector_store(embeds_path, knowledge_base_files)
+def main():
+    knowledge_bas_path = Path("docs/resources")
+    embeds_path = knowledge_bas_path / "embeds"
 
-inference_time = time()
-llm = Ollama(model="mistral:latest")
-qa = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=vector_store.as_retriever(), 
-)
+    knowledge_base_files = get_pdf_files(knowledge_bas_path)
+    vector_store = load_vector_store(embeds_path, knowledge_base_files)
 
-response = qa.run("Give me a very brief summary about the Phase 1 in the classification criteria topic")
-print(response)
-print(f"Inference time: {time() - inference_time:.2f} seconds")
+    llm = Ollama(model="mistral:latest")
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vector_store.as_retriever(), 
+    )
+
+    while True:
+        query = input("Enter your query (type 'exit', 'q' or '/exit' to quit): ")
+        if query.lower() in ['exit', 'q', '/exit']:
+            break
+        response = interact_with_pdfs(qa, query)
+        print(f'Response: {response}')
+
+if __name__ == '__main__':
+    main()
